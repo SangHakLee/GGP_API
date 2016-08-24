dfbcab260fd798f368acb22569dea71c7a755414
GM1702846 <GM1702846>
2016-08-24 11:58:13 +0900

    커밋 취소

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var models = require('../models');
+var config = require(__dirname + '/../config/firebase.json');
+var firebase = require('firebase');
+var _ = require('underscore');
 
+// var cons = require('../config/constant');
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCQ7ElGgbPhf8ueajaho8Bc2P-h8I-XW5U",
+  authDomain: "ggp10.firebaseapp.com",
+  databaseURL: "https://ggp10.firebaseio.com",
+  storageBucket: "firebase-ggp10.appspot.com",
+  serviceAccount: config
+});
+
+var models = require('../models');
 var logger = require('../logger/winston');
 
 /* GET users listing. */
@@ -17,6 +31,36 @@ router.get('/', function(req, res, next) {
   });
   // res.send('respond with a resource');
 });
+
+router.post('/firebase', function(req, res){
+  logger.info('post firebase');
+  var email = _.random(1, 10) + "@good.com";
+  var password = "123qwe";
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function(result){
+    console.log('result :', result);
+  })
+  .catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log('error :', error);
+  });
+
+});
+
+router.get('/firebase', function(req, res){
+  var user = firebase.auth().currentUser;
+  if (user) {
+  // User is signed in.
+    console.log(user);
+  } else {
+    // console.log(con.FIREBASE_DEFAULT_PASS);
+  // No user is signed in.
+  }
+
+  res.json(user);
+});
+
 
 
 
