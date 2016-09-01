7794779490c974f898d6b82707b87757d7598fa8
GM1702846 <GM1702846>
2016-09-01 17:58:33 +0900

    게시판 id 가져와 1개 처리. 현재 게시판과 db 상 게시판 차이 개수 만큰 게시글 가져오는 로직 필요

--- crawlers/boardCrawler2.js
+++ crawlers/boardCrawler2.js
@@ -6,18 +6,50 @@ var request = require('request'),
 var models = require('../models');
 
 
-function Crawler(){
+function Crawler(){}
 
+function getBoardNoFromUrl(url) {
+  var aa = url.split(';');
+  for (var i in aa) {
+    if (~aa[i].indexOf('board_no')) {
+      return aa[i].replace(/[^0-9]/g, '');
+    }
+  }
+  return null;
 }
 
-Crawler.prototype.getNowPostNumByBoardId = function(board_id){
-  this.getAllBoard(boa)
-  .then(function(){
-
-  })
-  .catch(function(){
-
-  });
+Crawler.prototype.getNowPostNumByBoardUrl = function(board_url){
+	var options = {
+    uri: board_url,
+    transform: function(body) {
+      return cheerio.load(body, {
+        normalizeWhitespace: true, // 공백 제거
+      });
+    }
+  };
+  return rp(options)
+    .then(function($) {
+      var now_post_no = null;
+      var now_board_no = null;
+      $('.boardlist tbody tr').each(function() {
+        var td = $(this).children().text();
+        var a = $(this).children().next(); // a 태그 선택자
+        $('img').remove();
+        td = td.split(' ')[0]; // 띄어쓰기로 자른 데이터
+        td = td.replace(/[^0-9]/g, ''); // 숫자만 가져오기
+        if (td) { // 공백 데이터 제외
+          now_board_no = getBoardNoFromUrl(a.eq(0).html());
+          now_post_no = td;
+          return false;
+        }
+      });
+      // console.log('now_post_no :',now_post_no);
+      return [now_post_no, now_board_no];
+    })
+    .catch(function(err) {
+			console.error('err: ', err);
+      throw new Error(err);
+    });
 };
 
 
@@ -38,6 +70,7 @@ Crawler.prototype.getAllBoard = function(board_no){
 };
 
 Crawler.prototype.getPostByCrawler = function(board_id, board_url, board_no, post_no){
+	console.log("hi getPostByCrawler");
   var options = {
 		uri: board_url,
 		qs : {
@@ -72,8 +105,8 @@ Crawler.prototype.getPostByCrawler = function(board_id, board_url, board_no, pos
     row.board_id = board_id;
     row.board_no = board_no;
 		row.post_no = post_no;
-		console.log('row : ', row);
-		// models.POSTS_STATS.create(row)
+		// console.log('row : ', row);
+		console.log('게시판 추가 : ', board_no);
 	})
 	.catch(function(err){
     console.error('err: ', err);
@@ -81,8 +114,46 @@ Crawler.prototype.getPostByCrawler = function(board_id, board_url, board_no, pos
 	});
 };
 
+Crawler.prototype.updateBoardById = function(board_id){
+
+	return models.Boards.findById(board_id)
+	.then(function(result){
+		// console.log(result);
+
+		crawler.getNowPostNumByBoardUrl(result.board_url)
+		.then(function(now){
+			console.log('now', now);
+		})
+
+		var _result = {
+			"board_id" : result.board_id,
+			"board_url" : result.board_url,
+			"now_board_no" : result.now_board_no,
+			"now_post_no" : result.now_post_no
+		};
+		return _result;
+	})
+	.catch(function(err){
+		throw new Error(err);
+	});
+};
+
+
+
 var crawler = new Crawler();
-// crawler.getPostByCrawler('http://gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358', 5671, 5177);
+// crawler.getPostByCrawler(1, 'http://gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358', 5700, 5204);
+crawler.updateBoardById(1)
+.then(function(result){
+	// return this.getPostByCrawler(1, result.board_url, result., 5204);
+	return crawler.getPostByCrawler(1, 'http://gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358', 5700, 5204);
+})
+.then(function(result){
+	console.log('result ?', result);
+})
+.catch(function(err){
+	console.log('err : ', err);
+});
+
 
 
 module.exports = Crawler;
