0ca6b45aab2b603cc417ab3c93f74cb3ff5966dd
SangHakLee <leesanghak7298@gmail.com>
2016-11-02 01:15:55 +0900

    공지사항 등록 후 이메일 보내기 완료

--- models/posts.js
+++ models/posts.js
@@ -33,7 +33,49 @@ module.exports = function(sequelize, DataTypes) {
         freezeTableName: true,
         underscored: true,
         classMethods: {},
-        hooks: {}
+        hooks: {
+			afterCreate: function(post, options) {
+
+				// FIXME
+				var DETAIL_URL = 'http://gachon.ac.kr/community/opencampus/03.jsp?mode=view&boardType_seq=358&board_no=';
+				var post_title = post.get('post_title');
+				var board_no   = post.get('board_no');
+				var url = DETAIL_URL + board_no;
+				send({
+					to: to,
+				},{
+					post_title: post_title,
+					link: link
+				},
+				function(err, info){
+					if(err){
+						logger.error(err)
+				   }else{
+				       console.log('Password reset sent');
+				   }
+			   });
+			}
+		}
     });
     return Posts;
 };
+
+var logger = require('../logger/winston');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var config = require(__dirname + '/../config/mail.json');
+var to = 'code.ryan.lee@gmail.com, ngcmw9101@gmail.com, prhkhk61@gmail.com';
+var smtpTransport = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    auth: config.auth
+}));
+
+
+var path = __dirname + '/../templates/post.html';
+var html =  fs.readFileSync(path, 'utf8');
+
+var send = smtpTransport.templateSender({
+	from: '아마따 <gachon10.bridge@gmail.com>',
+	subject: '공지사항 등록 안내',
+	html: html
+});
